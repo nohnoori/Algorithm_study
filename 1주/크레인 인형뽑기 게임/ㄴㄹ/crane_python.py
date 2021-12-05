f4d2d3cb24106a75d8ef1b4a91f5d14998892372
nohnoori <69712183+nohnoori@users.noreply.github.com>
2021-12-05 21:16:11 +0900

    크레인_파이썬

--- 1주/크레인 인형뽑기 게임/ㄴㄹ/crane_python.py
+++ 1주/크레인 인형뽑기 게임/ㄴㄹ/crane_python.py
@@ -1,14 +1,15 @@
-
 def solution(board, moves):
-    answer = []
-    cnt=0
-    for i in moves:
-        for j in range(len(board)):
-            if board[j][i-1]!=0:
-                answer.append(board[j][i-1])
-                board[j][i-1]=0
+    answer = 0
+    basket=[]
+    
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move-1]:
+                basket.append(board[i][move-1])
+                board[i][move-1]=0
                 break
-        if len(answer)>1 and answer[-1]==answer[-2]:
-            cnt+=2
-            del answer[-2:]
-    return cnt
+        if len(basket)>1 and basket[-1]==basket[-2]:
+            answer+=2
+            del basket[-2:]
+    
+    return answer
